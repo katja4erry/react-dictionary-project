ce93af9ac73fa7de4d1733bd029e93d759a826b4
katja4erry <106451546+katja4erry@users.noreply.github.com>
2022-10-09 09:23:37 +0200

    add synonyms and fixed example

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,16 +1,26 @@
 import React from "react";
+import Example from "./Example";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
+      <ul>
+        {props.meaning.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>{definition.definition}</p>
             <br />
-            <em>{definition.example}</em>
+            <p>
+              <strong>Definition: </strong>
+              {definition.definition}
+            </p>
+            <br />
+            <Example example={definition.example} />
           </div>
         );
       })}
